bce8ad03e786a73f5c5c0d1ae1e1fce1b8ef7cdc
robson <robson@dinfo-robson>
2013-12-03 09:54:03 -0300

    laboratório 02

--- fourlinux/src/test/java/br/com/fourlinux/laboratorio02/Teste.java
+++ fourlinux/src/test/java/br/com/fourlinux/laboratorio02/Teste.java
@@ -4,8 +4,6 @@
  */
 package br.com.fourlinux.laboratorio02;
 
-import br.com.fourlinux.laboratorio02.Funcionario;
-
 /**
  *
  * @author robson
@@ -13,8 +11,37 @@ import br.com.fourlinux.laboratorio02.Funcionario;
 public class Teste {
     public static void main (String[] args){
         Funcionario func = new Funcionario();
+        func.setId(01);
         func.setNome("Robson");
+        func.setCpf(123456789);
+        func.setRg("123456");
+        func.setTelCel(99998888);
+        func.setTelResid(32165498);
+        func.setEmail("robson@4linux.com.br");
+        func.setEndereco("Rod. Jk");
+        func.setCep(234564554);
+        func.setBairro("Zerão");
+        func.setCidade("Macapá");
+        func.setUf("AP");
+        func.setUsuario("robson");
+        func.setSenha("123456");
         System.out.println(func.getNome());
+        
+        
+        Servico serv = new Servico();
+        serv.setId(01);
+        serv.setFuncionarioResp(null);
+        serv.setCliente("Cliente 01");
+        
+        serv.setEnderecoColeta("Av. Raimundo Peres, 613");
+        serv.setEnderecoEntrega("Av. FAB, 1256");
+        serv.setDistanciaKm(25.10);
+        serv.setAltura(5.14);
+        serv.setLargura(3.37);
+        serv.setComprimento(7.35);
+        serv.setPeso(20.50);
+        System.out.println(serv.getCliente());
+        
     }
     
    
