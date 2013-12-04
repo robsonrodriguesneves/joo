e4f9db4b440e5747b7d4c9e006cf0a91e4cb6e2b
robson <robson@dinfo-robson>
2013-12-04 17:33:38 -0300

    laboratório 02

--- fourlinux/src/test/java/br/com/fourlinux/laboratorio02/Teste.java
+++ fourlinux/src/test/java/br/com/fourlinux/laboratorio02/Teste.java
@@ -5,14 +5,18 @@
 package br.com.fourlinux.laboratorio02;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import javax.xml.crypto.Data;
 
 /**
  *
  * @author robson
  */
 public class Teste {
-    public static void main (String[] args){
+    public static void main (String[] args) throws ParseException{
         Funcionario func = new Funcionario();
         func.setId(01);
         func.setNome("Robson");
@@ -54,5 +58,25 @@ public class Teste {
         System.out.println("Data: "+formataData.format(serv.getData()));
         //System.out.println("Hora: "+serv.getHora());
         System.out.println("Hora: "+formataHora.format(serv.getHora()));
+        
+        
+        
+        
+        Calendar c = Calendar.getInstance();
+		Date data = c.getTime();
+		DateFormat f = DateFormat.getDateInstance();
+		
+		Date data2 = f.parse("12/01/1995");
+		System.out.println(data2);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Data formatada: "+sdf.format(data));
+		
+		//Converte Objetos
+		System.out.println("Data convertida: "+sdf.parse("02/08/1970"));
+
+
+        
+        
     }
 }
